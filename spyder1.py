41dd8dbc7084ad16de49d0542a96553b65fa5a5c
Adedamola Yusuf <adedamolayusuf@Adedamolas-MacBook-Pro.local>
2021-07-25 14:53:49 +0200

    spyder 1

---
+++ spyder1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 25 14:48:34 2021
+
+@author: adedamolayusuf
+"""
+
+import pandas as pd
+sales =pd.Series ([10,25,30,2])
+sales
